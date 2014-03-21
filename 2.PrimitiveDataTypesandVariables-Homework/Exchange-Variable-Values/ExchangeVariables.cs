4ac5f7fbb4794e9908fb1f15d5396ee05a951b94
vvalkov <velizar90@gmail.com>
2014-03-21 17:21:20 +0200

    Isosceles-Triangle and Exchange-Variable-Values working.

---
+++ 2.PrimitiveDataTypesandVariables-Homework/Exchange-Variable-Values/ExchangeVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ExchangeVariables
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+
+        Console.WriteLine("Beofre exchange: {0} {1}", a, b);
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        Console.WriteLine("After exchange: {0} {1}", a, b);
+
+    }
+}
